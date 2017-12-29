496079c3e592836f9e6e726007e8ebdb9881a885
ACoded <1327320916@qq.com>
2017-12-29 14:56:12 +0800

    Add files via upload

--- src/components/login.vue
+++ src/components/login.vue
@@ -50,13 +50,7 @@
 </template>
 
 <script>
-  //http://10.254.2.17:8080/platform/frame/dmwh/dmAction.do?yhid=yangj&yhmm=f408e260b87a089b925516846cebe59c
-  //<input type="hidden" name="msg" value="">
-  //<input name="yhmm" type="password" class="text" value="" style="width: 90%;" onkeydown="keyDown(event);">
 
-  //http://10.254.2.17:8080/platform/ClusterRemoteServlet  action_sso_ask_create_ticket
-
-  //header:{FRAME_SSO_TICKET: ticket}
   import Cookies from "js-cookie"
 
   export default {
@@ -81,25 +75,9 @@
         this.$refs.loginForm.validate((valid) => {
           if (valid) {
             if (!Cookies.get('ticket')) {
-              this.$http.post({
-                url: '/api/Dzpj/ClientActionServlet',
-                actionHandler: "com.lanxum.dzpj.application.login.handler.LoginHandler",
-                actionMethod: "login",
-                data: this.form
-              }).then((res) => {
-                Cookies.set('ticket', res.body.ticket);
-                this.$store.commit('initNetWork', {ticket: res.body.ticket});
-              }).catch((err) => {
-                console.log(err, 222)
-              })
-            }
-          }
-          /*else {
-            this.$Message.error('')
-          }*/
-        })
+
       }
-    }
+    }})}}
   }
 </script>
 
