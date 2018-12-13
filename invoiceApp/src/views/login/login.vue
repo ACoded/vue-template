879332ec18dd73239c7b95b4fe1203fed50440ae
ACoded <1327320916@qq.com>
2018-12-13 17:39:34 +0800

    Update login.vue

--- invoiceApp/src/views/login/login.vue
+++ invoiceApp/src/views/login/login.vue
@@ -20,28 +20,12 @@
       </grid-item>
     </grid>
   </div>
-  <!--
-  网络请求方式
-    this.$http.post({
-    url:'',
-    actionHandler: "",
-    actionMethod: "",
-    data: {},
-    ContentType:""  //选填
-    })
--->
+
 
 </template>
 
 <script>
 
-  //http://10.254.2.17:8080/platform/frame/dmwh/dmAction.do?yhid=yangj&yhmm=f408e260b87a089b925516846cebe59c
-  //<input type="hidden" name="msg" value="">
-  //<input name="yhmm" type="password" class="text" value="" style="width: 90%;" onkeydown="keyDown(event);">
-
-  //http://10.254.2.17:8080/platform/ClusterRemoteServlet  action_sso_ask_create_ticket
-
-  //header:{FRAME_SSO_TICKET: ticket}
   import {Grid,GridItem, XHeader,GroupTitle, Group, XInput} from 'vux'
   import Cookies from "js-cookie"
   export default {
@@ -67,33 +51,7 @@
     },
     methods: {
       login() {
-            if (!Cookies.get('ticket')) {
-              this.$http.post({
-                url: 'http://10.254.2.68:8090/Dzpj/ClientActionServlet',
-                actionHandler: "com.lanxum.dzpj.application.login.handler.LoginHandler",
-                actionMethod: "login",
-                data: this.form
-              }).then((res) => {
-                Cookies.set('ticket', res.body.ticket);
-                this.$store.commit('initNetWork', {ticket: res.body.ticket});
-                this.$store.commit('setName', this.form.yhid);
-                this.$router.push({path: '/home'})
-              }).catch((err) => {
-                this.$vux.alert.show({
-                  title: 'login',
-                  content:err,
-                  onShow () {
-                    console.log('Plugin: I\'m showing')
-                  },
-                  onHide () {
-                    console.log('Plugin: I\'m hiding')
-                  }
-                })
-              })
-          }
-          /*else {
-            this.$Message.error('')
-          }*/
+           
       }
     }
   }
