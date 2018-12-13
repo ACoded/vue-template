22df37fc2ab762de8515ce8f9e20ce454b646ecb
ACoded <1327320916@qq.com>
2018-12-13 17:40:11 +0800

    Update signin.vue

--- invoiceApp/src/views/login/signin.vue
+++ invoiceApp/src/views/login/signin.vue
@@ -23,31 +23,8 @@
     },
     methods: {
       register() {
-        this.$http.post({
-          url: 'http://10.254.2.68:8090/Dzpj/ClientActionServlet',
-          actionHandler: "com.lanxum.dzpj.application.login.handler.RegisterHandler",
-          actionMethod: "register",
-          data: this.form
-        }).then((res) => {
-          console.log(res);
-//              this.$vux.alert('注册成功，请前往登录！');
-          this.$router.push({path: '/login', query: this.form});
-        }).catch((err) => {
-          this.$vux.alert.show({
-            title: 'singin',
-            content: err,
-            onShow() {
-              console.log('Plugin: I\'m showing')
-            },
-            onHide() {
-              console.log('Plugin: I\'m hiding')
-            }
-          })
-        })
-        /*else {
-         this.$Message.error('')
-         }*/
-      }
+       
+    }
     }
   }
 </script>
