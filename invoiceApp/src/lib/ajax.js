d3ce41cad6424258de4323ec88f633fc8226094e
ACoded <1327320916@qq.com>
2018-12-13 17:38:40 +0800

    Update ajax.js

--- invoiceApp/src/lib/ajax.js
+++ invoiceApp/src/lib/ajax.js
@@ -5,54 +5,16 @@ axios.defaults.timeout = '5000';
 axios.defaults.headers.post['Content-Type'] = "application/json;charset=utf-8";
 
 const NetWork = {};
-let data1 = {};
-let result = {};
-NetWork.Success = "Success";//成功
-NetWork.initData = function (obj) {
-  console.log(obj,1111)
-  data1 = {
-    ClientMsg: {
-      RequestAction: {
-        Type: obj.actionHandler||'',
-        Action: obj.actionMethod||'',
-      },
-      Body: obj.data ||''
-    }
-  };
-  /* zip.generateAsync({type: "base64"}).then(function (res) {
-     this.data = res;
-   });
- */
-  this.data =data1;
-  this.url = obj.url;
-  this.ContentType = obj.ContentType ||  "application/json;charset=utf-8";
 
-};
 
 NetWork.post = function (obj) {
-  this.initData.call(this, obj);
-  axios.defaults.headers['content-type']=this.ContentType;
+ 
+  axios.defaults.headers['content-type']=obj.ContentType;
   return new Promise((resolve, reject) => {
-    axios.post(NetWork.url, JSON.stringify(NetWork.data))
+    axios.post(obj.url, JSON.stringify(obj.data))
       .then((res) => {
       console.log('success',111)
-        if (res.data.ServerMsg.ReturnMsgCode == 0) {
-
-          if (res.data.ServerMsg.RtnBizBody.RtnBizCode == NetWork.Success) {
-            result.body = res.data.ServerMsg.RtnBizBody.Body;
-
-            console.log(result,4445555,res.data.ServerMsg)
-            resolve(result)
-          } else {
-            result.code = res.data.ServerMsg.RtnBizBody.RtnBizCode;
-            result.msg = res.data.ServerMsg.RtnBizBody.RtnBizReason;
-            result.shortMsg = res.data.ServerMsg.RtnBizBody.RtnBizMessage;
-            reject(result)
-          }
-        } else {
-          result.msg = res.data.ServerMsg.RtnBizBody.RtnBizReason;
-          reject(result)
-        }
+        
       }).catch((err) => {
       result.msg ='请求发送失败，请联系!'+err;
       reject(result)
